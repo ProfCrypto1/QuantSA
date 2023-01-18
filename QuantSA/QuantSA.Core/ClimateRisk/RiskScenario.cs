63bb7ff7413791e37bed5d79b95ba3f0cf74f89f
ProfCrypto1 <77839197+ProfCrypto1@users.noreply.github.com>
2023-01-18 12:14:29 +0200

    Test

---
+++ QuantSA/QuantSA.Core/ClimateRisk/RiskScenario.cs
@@ -0,0 +1,16 @@
+﻿using Accord.Statistics.Distributions.Univariate;
+using QuantSA.Core.Products.Rates;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace QuantSA.Core.ClimateRisk
+{
+    public class RiskScenario
+    {
+        public double InterestRateScenario(double interestRate)
+        {
+            return 2.0;
+        }
+    }
+}
